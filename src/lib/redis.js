b889c916afd2e036eaef9afadf12a1d6ed922df8
Fernando Buccella <fer.buccella@gmail.com>
2021-09-15 20:33:48 -0300

    Disable offline queue for redis commands

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -4,7 +4,10 @@ const redis = require('redis');
 const { REDIS_URL } = require('../config');
 const logger = require('../logger');
 
-const client = redis.createClient({ url: REDIS_URL });
+const client = redis.createClient({
+  url: REDIS_URL,
+  enable_offline_queue: false,
+});
 
 client.on('error', (err) => {
   logger.error('Redis error: %o', err);
